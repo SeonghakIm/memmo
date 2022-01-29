c2ffa5912f0a50dd7549eda6598be5a34e6765eb
SeonghakIm <tjdgkr140@naver.com>
2022-01-29 21:22:19 +0900

    서버 회원가입 복붙

---
+++ server/index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { auth } = require("./middleware/auth");
+const { User } = require("./models/User");
+const config = require("./config/key");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+mongoose
+  .connect(config.mongoURI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello world!"));
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+  });
+
+  user.comparePassword(req.body.password, (err, isMatch) => {
+    if (!isMatch)
+      return res.json({
+        loginSuccess: false,
+        message: "비밀번호가 틀렸습니다.",
+      });
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res
+        .cookie("x_auth", user.token)
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    nickName: req.user.nickName,
+    role: req.user.role,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
+
+const port = 5000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
