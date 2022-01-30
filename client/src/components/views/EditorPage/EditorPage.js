6ea838fb5639fcffed39be90826a0de42e88b6fa
thefrontD_labtop <amigojmk@gmail.com>
2022-01-30 22:23:40 +0900

    editor 진행중
    아직 제대로 작동 안함

--- client/src/components/views/EditorPage/EditorPage.js
+++ client/src/components/views/EditorPage/EditorPage.js
@@ -11,11 +11,25 @@ function LoginPage(props) {
   const [Title, setTitle] = useState("");
   const [Content, setContent] = useState("");
 
+  /*let doBold = document.execCommand("Bold");
+  let doItalic = document.execCommand("Italic"); // 기울이기
+  let doUnderline = document.execCommand("Underline"); // 밑줄
+  let doStrikeThrough = document.execCommand("StrikeThrough"); // 중간줄
+  let doCut = document.execCommand("Cut"); // 자르기
+  let doCopy = document.execCommand("Copy"); // 복사하기
+  let doPaste = document.execCommand("Paste"); // 붙혀넣기
+  let doUndo = document.execCommand("Undo"); // 실행취소
+  let doRedo = document.execCommand("Redo"); // 다시실행
+  let doinsertorderedList = document.execCommand("insertorderedList"); // 글번호 매기기
+  let doinsertunorderdList = document.execCommand("insertunorderdList"); // 글머리 매기기
+  let dooutdent = document.execCommand("outdent"); // 내어쓰기
+  let doindent = document.execCommand("indent"); // 들여쓰기
+  let dojustifyleft = document.execCommand("justifyleft"); // 왼쪽정렬
+  let dojustifycenter = document.execCommand("justifycenter"); // 가운데정렬
+  let dojustifyright = document.execCommand("justifyright"); // 오른쪽정렬*/
+
   const onTitleHandler = (event) => {
     setTitle(event.currentTarget.value);
-  };
-  const onContentHandler = (event) => {
-    setContent(event.currentTarget.value);
   };
   const onSubmitHandler = (event) => {
     event.preventDefault();
@@ -38,17 +52,46 @@ function LoginPage(props) {
   return (
     <div
       style={{
-        display: "flex",
         justifyContent: "center",
         alignItems: "center",
         width: "100%",
         height: "100vh",
       }}
     >
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={onSubmitHandler}
-      >
+      <div>
+        <input type="button" value="B" onclick={document.execCommand("bold")} />
+        <input
+          type="button"
+          value="Italic"
+          onclick={document.execCommand("Italic")}
+        />
+        <input
+          type="button"
+          value="abc"
+          onclick={document.execCommand("Underline")}
+        />
+        <input
+          type="button"
+          value="abc"
+          onclick="document.execCommand('StrikeThrough')"
+        />
+        <input
+          type="button"
+          value="왼쪽 정렬"
+          onclick="document.execCommand('justifyleft')"
+        />
+        <input
+          type="button"
+          value="가운데 정렬"
+          onclick="document.execCommand('justifycenter')"
+        />
+        <input
+          type="button"
+          value="오른쪽 정렬"
+          onclick="document.execCommand('justifyright')"
+        />
+      </div>
+      <div>
         <label>작성자</label>
         <label value={Name} />
         <br />
@@ -56,16 +99,10 @@ function LoginPage(props) {
         <input type="text" value={Title} onChange={onTitleHandler} />
         <br />
         <label>글 내용</label>
-        <br />
-        <textarea
-          type="text"
-          cols={100}
-          rows={10}
-          value={Content}
-          onChange={onContentHandler}
-        />
-        <button>작성</button>
-      </form>
+        <div contentEditable="true"></div>
+        <div></div>
+      </div>
+      <button>작성</button>
     </div>
   );
 }
