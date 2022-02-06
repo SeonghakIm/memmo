1a20652b73f1384d3b882958dd3790b886d60bcb
thefrontD_labtop <amigojmk@gmail.com>
2022-02-06 23:04:23 +0900

    login오류수정
    중괄호 위치가 잘못됨

--- server/index.js
+++ server/index.js
@@ -43,22 +43,26 @@ app.post("/api/users/login", (req, res) => {
         message: "제공된 이메일에 해당하는 유저가 없습니다.",
       });
     }
-  });
 
-  user.comparePassword(req.body.password, (err, isMatch) => {
-    if (!isMatch)
-      return res.json({
-        loginSuccess: false,
-        message: "비밀번호가 틀렸습니다.",
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
       });
-    user.generateToken((err, user) => {
-      if (err) return res.status(400).send(err);
-      res
-        .cookie("x_auth", user.token)
-        .status(200)
-        .json({ loginSuccess: true, userId: user._id });
     });
   });
+});
+
+app.get("/api/hello", (req, res) => {
+  res.send("안녕하세요~");
 });
 
 app.get("/api/users/auth", auth, (req, res) => {
